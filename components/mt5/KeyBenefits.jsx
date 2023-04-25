7e930719b375d1e38a4c4a6766ade3836631928f
andinianst93 <andinianst93@gmail.com>
2023-04-25 16:46:06 +0700

    asda

--- components/mt5/KeyBenefits.jsx
+++ components/mt5/KeyBenefits.jsx
@@ -43,7 +43,7 @@ const KeyBenefits = () => {
   return (
     <section className='bg-black py-20 sm:py-32'>
       <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-        <div className='mx-auto max-w-2xl lg:text-center'>
+        <div className='max-w-2xl lg:text-left'>
           <h2 className='text-3xl font-medium tracking-tight text-white'>
             Key Features of MetaTrader 5
           </h2>
